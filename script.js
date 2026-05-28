7489104a6a1be8345b3920544fbf6d53412d6537
sesaymamud65-ops <sesaymamud65@gmail.com>
2026-05-28 07:50:59 +0000

    test

---
+++ script.js
@@ -0,0 +1,7 @@
+﻿const navToggle = document.querySelector('.nav-toggle');
+const siteNav = document.querySelector('.site-nav');
+
+navToggle?.addEventListener('click', () => {
+  const isOpen = siteNav.classList.toggle('open');
+  navToggle.setAttribute('aria-expanded', String(isOpen));
+});
